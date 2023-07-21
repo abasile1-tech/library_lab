8cb75d613a19b776e8bc34350240c6e18dedb7a7
John <ashton.basile@gmail.com>
2023-07-21 15:22:22 +0100

    I made some basic setup and am starting to write tests first in a test-driven manner

---
+++ models/library.py
@@ -0,0 +1,15 @@
+class Library:
+	def __init__(self, books):
+		self.books = books
+
+	def list_all_books(self):
+		pass
+
+	def show_individual_book(self):
+		pass
+
+	def add_new_book_to_library(self):
+		pass
+
+	def remove_book_from_library(self):
+		pass
---
+++ tests/library_test.py
@@ -0,0 +1,22 @@
+import unittest
+from models.book import Book
+from models.library import Library
+
+class TestBook(unittest.TestCase):
+	def setUp(self):
+		self.philoshophers_stone = Book("Harry Potter and the Philosoper's Stone", "J.K. Rowling", "Fantasy")
+		self.programming_textbook = Book("Programming: Principles and Practice Using C++", "Bjarne Stroustrup", "Educational")
+		self.hobbit = Book("The Hobbit", "J.R.R. Tolkien", "Fantasy")
+		self.my_library = Library([self.philoshophers_stone, self.programming_textbook, self.hobbit])
+
+	def test_list_all_books(self):
+		self.assertEqual()
+
+	def test_show_individual_book(self):
+		pass
+
+	def test_add_new_book_to_library(self):
+		pass
+
+	def test_remove_book_from_library(self):
+		pass
